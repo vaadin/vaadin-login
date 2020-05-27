6a1f731ff0fa36e22fe5e378b9326cd6b51dffe8
web-padawan <iamkulykov@gmail.com>
2020-05-27 14:29:36 +0300

    feat: prepare for TS defs generation [skip ci]

---
+++ @types/interfaces.d.ts
@@ -0,0 +1,13 @@
+export interface LoginI18n {
+  form: {
+    title: string;
+    username: string;
+    password: string;
+    submit: string;
+    forgotPassword: string;
+  };
+  errorMessage: {
+    title: string;
+    message: string;
+  };
+}
