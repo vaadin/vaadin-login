7b3798cd1e8f81b4cfc8236d71b141769599ff5e
web-padawan <iamkulykov@gmail.com>
2020-12-23 13:08:28 +0200

    Replace dom-module with registerStyles

--- theme/lumo/vaadin-login-form-styles.js
+++ theme/lumo/vaadin-login-form-styles.js
@@ -1,14 +1,13 @@
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
 
-const $_documentContainer = html`<dom-module id="lumo-login-form" theme-for="vaadin-login-form">
-  <template>
-    <style>
-      vaadin-button[part="vaadin-login-submit"] {
-        margin-top: var(--lumo-space-l);
-        margin-bottom: var(--lumo-space-s);
-      }
-    </style>
-  </template>
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
+registerStyles(
+  'vaadin-login-form',
+  css`
+    vaadin-button[part='vaadin-login-submit'] {
+      margin-top: var(--lumo-space-l);
+      margin-bottom: var(--lumo-space-s);
+    }
+  `,
+  { moduleId: 'lumo-login-form' }
+);
--- theme/material/vaadin-login-form-styles.js
+++ theme/material/vaadin-login-form-styles.js
@@ -1,24 +1,21 @@
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 
-const $_documentContainer = html`<dom-module id="material-login-form" theme-for="vaadin-login-form">
-  <template>
-    <style>
-      vaadin-button[part="vaadin-login-submit"] {
-        margin-top: 3em;
-        margin-bottom: 2em;
-        flex-grow: 0;
+registerStyles(
+  'vaadin-login-form',
+  css`
+    vaadin-button[part='vaadin-login-submit'] {
+      margin-top: 3em;
+      margin-bottom: 2em;
+      flex-grow: 0;
+    }
+
+    /* Small screen */
+    @media only screen and (max-width: 1023px) {
+      vaadin-button[part='vaadin-login-submit'] {
+        margin-top: 2.5em;
+        margin-bottom: 1em;
       }
-
-      /* Small screen */
-      @media only screen
-        and (max-width: 1023px) {
-        vaadin-button[part="vaadin-login-submit"] {
-          margin-top: 2.5em;
-          margin-bottom: 1em;
-        }
-      }
-    </style>
-  </template>
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
+    }
+  `,
+  { moduleId: 'material-login-form' }
+);
